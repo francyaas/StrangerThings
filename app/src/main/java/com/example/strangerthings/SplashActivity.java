4fa7f2f5c62416dea70397b0d31b6e3320cb0a2e
Yasmin Francisquetti <66258704+yasminfrancisquetti@users.noreply.github.com>
2021-03-22 07:57:03 -0300

    Continuação do progresso na personalização das telas

---
+++ app/src/main/java/com/example/strangerthings/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.strangerthings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread(() -> {
+            /*
+            try
+            {
+                Thread.sleep(10000); // this 1000 is milliseconds
+                //Intent starter = new Intent(this, MainActivity.class);
+                //startActivity(starter);
+            }
+            catch (InterruptedException ex)
+            {
+                Log.e("waffles", "sleep interrupted, welcome to etec", ex);
+            }
+             */
+        }
+        ).start();
+
+    }
+}
