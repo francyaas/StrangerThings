11a2e975ee5d9dc873760bf6cb27c1d63803b91d
Yasmin Francisquetti <66258704+yasminfrancisquetti@users.noreply.github.com>
2021-03-22 07:44:22 -0300

    Progresso na personalização das telas

--- app/src/main/java/com/example/strangerthings/MainActivity.java
+++ app/src/main/java/com/example/strangerthings/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.strangerthings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText    editTextCharacterName = findViewById(R.id.editTextCharacterName);
+    }
+
+    public void Search(View buttonSearch)
+    {
+        Intent starter = new Intent(this, SplashActivity.class);
+        startActivity(starter);
+
     }
 }
